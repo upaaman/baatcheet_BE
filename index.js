83727e87d2103dbadb8ec0b00fa25432750a4753
upaaman <upadhyayaman9685@gmail.com>
2024-06-26 18:16:47 +0530

    Revert "completed the project"
    This reverts commit 46fb5f6e09daca9c65dca2339c40a167418cad42.

--- index.js
+++ index.js
@@ -1,65 +1,31 @@
 const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
-const dotenv = require('dotenv');
+const User = require("./models/User");
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
-const User = require('./models/User');
+const ws = require("ws")
+//to get the data from env file
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
 const Message = require('./models/Message');
-const ws = require('ws');
-const fs = require('fs');
-
 dotenv.config();
-mongoose.connect(process.env.MONGO_URL, (err) => {
-    if (err) throw err;
-});
+app.use(express.json());
+app.use(cookieParser());
+mongoose.connect(process.env.MONGO_URL);
 const jwtSecret = process.env.JWT_SECRET;
 const bcryptSalt = bcrypt.genSaltSync(10);
 
-const app = express();
-app.use('/uploads', express.static(__dirname + '/uploads'));
-app.use(express.json());
-app.use(cookieParser());
+
 app.use(cors({
     credentials: true,
     origin: process.env.CLIENT_URL,
-}));
+}))
+app.get("/test", (req, res) => {
+    res.send("Hello World");
+})
 
-async function getUserDataFromRequest(req) {
-    return new Promise((resolve, reject) => {
-        const token = req.cookies?.token;
-        if (token) {
-            jwt.verify(token, jwtSecret, {}, (err, userData) => {
-                if (err) throw err;
-                resolve(userData);
-            });
-        } else {
-            reject('no token');
-        }
-    });
-
-}
-
-app.get('/', (req, res) => {
-    res.json('test ok');
-});
-
-app.get('/messages/:userId', async (req, res) => {
-    const { userId } = req.params;
-    const userData = await getUserDataFromRequest(req);
-    const ourUserId = userData.userId;
-    const messages = await Message.find({
-        sender: { $in: [userId, ourUserId] },
-        recipient: { $in: [userId, ourUserId] },
-    }).sort({ createdAt: 1 });
-    res.json(messages);
-});
-
-app.get('/people', async (req, res) => {
-    const users = await User.find({}, { '_id': 1, username: 1 });
-    res.json(users);
-});
 
 app.get('/profile', (req, res) => {
     const token = req.cookies?.token;
@@ -87,11 +53,6 @@ app.post('/login', async (req, res) => {
         }
     }
 });
-
-app.post('/logout', (req, res) => {
-    res.cookie('token', '', { sameSite: 'none', secure: true }).json('ok');
-});
-
 app.post('/register', async (req, res) => {
     const { username, password } = req.body;
     try {
@@ -113,39 +74,16 @@ app.post('/register', async (req, res) => {
 });
 
 
-// const server = app.listen(port, () => console.log("Server Started at port ",port));
+const server = app.listen(4040, (req, res) => console.log("hiiii"));
 
-const server = app.listen(process.env.PORT);
+
 
 const wss = new ws.WebSocketServer({ server });
 wss.on('connection', (connection, req) => {
 
-    function notifyAboutOnlinePeople() {
-        [...wss.clients].forEach(client => {
-            client.send(JSON.stringify({
-                online: [...wss.clients].map(c => ({ userId: c.userId, username: c.username })),
-            }));
-        });
-    }
+    console.log("connected to WS")
 
-    connection.isAlive = true;
 
-    connection.timer = setInterval(() => {
-        connection.ping();
-        connection.deathTimer = setTimeout(() => {
-            connection.isAlive = false;
-            clearInterval(connection.timer);
-            connection.terminate();
-            notifyAboutOnlinePeople();
-            console.log('dead');
-        }, 1000);
-    }, 5000);
-
-    connection.on('pong', () => {
-        clearTimeout(connection.deathTimer);
-    });
-
-    // read username and id form the cookie for this connection
     const cookies = req.headers.cookie;
     if (cookies) {
         const tokenCookieString = cookies.split(';').find(str => str.startsWith('token='));
@@ -161,42 +99,34 @@ wss.on('connection', (connection, req) => {
             }
         }
     }
-
     connection.on('message', async (message) => {
         const messageData = JSON.parse(message.toString());
-        const { recipient, text, file } = messageData;
-        let filename = null;
-        if (file) {
-            console.log('size', file.data.length);
-            const parts = file.name.split('.');
-            const ext = parts[parts.length - 1];
-            filename = Date.now() + '.' + ext;
-            const path = __dirname + '/uploads/' + filename;
-            const bufferData = new Buffer(file.data.split(',')[1], 'base64');
-            fs.writeFile(path, bufferData, () => {
-                console.log('file saved:' + path);
-            });
-        }
-        if (recipient && (text || file)) {
+        const { recipient, text } = messageData;
+        if (recipient && text) {
             const messageDoc = await Message.create({
-                sender: connection.userId,
+                sender: connection.userId, 
                 recipient,
                 text,
-                file: file ? filename : null,
             });
-            console.log('created message');
             [...wss.clients]
                 .filter(c => c.userId === recipient)
                 .forEach(c => c.send(JSON.stringify({
                     text,
                     sender: connection.userId,
                     recipient,
-                    file: file ? filename : null,
-                    _id: messageDoc._id,
+                    id: messageDoc._id,
+
                 })));
         }
+
     });
 
-    // notify everyone about online people (when someone connects)
-    notifyAboutOnlinePeople();
-});
+    [...wss.clients].forEach(client => {
+        client.send(JSON.stringify({
+            online: [...wss.clients].map(c => ({ userId: c.userId, username: c.username })),
+        }));
+    });
+
+
+})
+//  f2Q9zpdrArlQgYZC
